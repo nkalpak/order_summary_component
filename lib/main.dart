ed80857e1039826855e7a8556611f03158c55a92
nkalpak <nikita.kalpakovski@outlook.com>
2022-02-12 11:23:18 +0100

    feat: add order summary card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,234 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'Red Hat Display',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+          color: CustomColors.paleBlue,
+          child: OrderSummaryCard(
+            child: Column(
+              children: [
+                OrderSummaryImage(
+                    child: SvgPicture.asset(
+                  'assets/images/illustration-hero.svg',
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.cover,
+                )),
+                Expanded(
+                    child: OrderSummaryContent(children: [
+                  const OrderSummaryTitle(title: 'Order Summary'),
+                  const OrderSummaryDescription(
+                      description:
+                          'You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!'),
+                  OrderSummaryTile(
+                      leading: SvgPicture.asset('assets/images/icon-music.svg'),
+                      title: Column(
+                        children: const [
+                          Text(
+                            'Annual Plan',
+                            style: TextStyle(fontWeight: FontWeight.w900),
+                          ),
+                          Text('\$59.99/year')
+                        ],
+                      ),
+                      trailing: Text(
+                        'Change',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: CustomColors.brightBlue,
+                            fontWeight: FontWeight.w700),
+                      )),
+                  const SizedBox(
+                    width: double.infinity,
+                    child: OrderSummaryButton(
+                      child: Text(
+                        'Proceed to Payment',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                        style: ButtonStyle(
+                            padding:
+                                MaterialStateProperty.all(const EdgeInsets.all(16))),
+                        onPressed: () => {},
+                        child: Text('Cancel Order',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: CustomColors.desaturatedBlue))),
+                  )
+                ])),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class OrderSummaryButton extends StatelessWidget {
+  final Widget child;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const OrderSummaryButton({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+            padding: MaterialStateProperty.all(const EdgeInsets.all(16)),
+            backgroundColor: MaterialStateProperty.resolveWith(
+              (states) {
+                return CustomColors.brightBlue;
+              },
+            )),
+        onPressed: () {},
+        child: child);
+  }
+}
+
+class OrderSummaryTile extends StatelessWidget {
+  final Widget leading, trailing, title;
+
+  const OrderSummaryTile(
+      {Key? key,
+      required this.leading,
+      required this.trailing,
+      required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: CustomColors.veryPaleBlue,
+      elevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: DefaultTextStyle.merge(
+          style: TextStyle(fontSize: 18, color: CustomColors.darkBlue),
+          child: Row(
+            children: [
+              leading,
+              const SizedBox(width: 16),
+              title,
+              const Spacer(),
+              trailing
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class OrderSummaryDescription extends StatelessWidget {
+  final String description;
+
+  const OrderSummaryDescription({required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      description,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: CustomColors.desaturatedBlue,
+        fontSize: 18,
+      ),
+    );
+  }
+}
+
+class OrderSummaryTitle extends StatelessWidget {
+  final String title;
+
+  const OrderSummaryTitle({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontWeight: FontWeight.w900,
+          fontSize: 28,
+          color: CustomColors.darkBlue),
+    );
+  }
+}
+
+class OrderSummaryCard extends StatelessWidget {
+  final Widget child;
+
+  const OrderSummaryCard({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8,
+      borderRadius: BorderRadius.circular(16),
+      child: child,
+    );
+  }
+}
+
+class OrderSummaryImage extends StatelessWidget {
+  final Widget child;
+
+  const OrderSummaryImage({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+      child: ClipRRect(
+        child: child,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+      ),
+    );
+  }
+}
+
+class OrderSummaryContent extends StatelessWidget {
+  final List<Widget> children;
+
+  const OrderSummaryContent({Key? key, required this.children})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: children,
+      ),
+    );
+  }
+}
+
+class CustomColors {
+  static Color paleBlue =
+      const HSLColor.fromAHSL(1.0, 225, 1.0, 0.94).toColor();
+  static Color brightBlue =
+      const HSLColor.fromAHSL(1.0, 245, 0.75, 0.52).toColor();
+  static Color veryPaleBlue =
+      const HSLColor.fromAHSL(1.0, 225, 1.0, 0.98).toColor();
+  static Color desaturatedBlue =
+      const HSLColor.fromAHSL(1.0, 224, 0.23, 0.55).toColor();
+  static Color darkBlue =
+      const HSLColor.fromAHSL(1.0, 223, 0.47, 0.23).toColor();
 }
